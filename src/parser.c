bee31edda60804a1dbaa612b65ac1a980f91b68a
Hunter Brady <redkast@gmail.com>
2018-07-29 11:29:38 -0700

    Basic expression and statement handling

--- src/parser.c
+++ src/parser.c
@@ -1,5 +1,415 @@
 #include "parser.h"
 
+/* Abstract Syntax Tree structures */
+struct expr_s;
+typedef struct expr_s expr_t;
+
+typedef struct
+{
+	expr_t *expression;
+} exprGroup_t;
+
+typedef struct
+{
+	token_t operator;
+	expr_t *right;
+} exprUnary_t;
+
+typedef struct
+{
+	token_t operator;
+	expr_t *left;
+	expr_t *right;
+} exprBinary_t;
+
+typedef struct
+{
+	token_t value;
+} exprLiteral_t;
+
+typedef enum
+{
+	EXPR_NONE,
+	EXPR_GROUP,
+	EXPR_UNARY,
+	EXPR_BINARY,
+	EXPR_LITERAL,
+} exprType_t;
+struct expr_s
+{
+	exprType_t type;
+	union
+	{
+		exprGroup_t group;
+		exprUnary_t unary;
+		exprBinary_t binary;
+		exprLiteral_t literal;
+	};
+};
+
+typedef struct
+{
+	expr_t *expr;
+} stmtExpr_t;
+
+typedef struct
+{
+	token_t name;
+	token_t type;
+	expr_t *initializer;
+} stmtVar_t;
+
+typedef enum
+{
+	STMT_NONE,
+	STMT_EXPR,
+	STMT_VAR,
+} stmtType_t;
+typedef struct
+{
+	stmtType_t type;
+	union
+	{
+		stmtExpr_t expression;
+		stmtVar_t var;
+	};
+} stmt_t;
+
+declareArrayOf(expr_t);
+declareArrayOf(stmt_t);
+
+/* Recursive descent parser */
+typedef struct
+{
+	u32 currentToken;
+	const char *code;
+	const token_t *tokens;
+
+	arrayOf(expr_t) expressions;
+	arrayOf(stmt_t) statements;
+} parser_t;
+
+static void error(const parser_t *parser, token_t token, const char *msg)
+{
+	printf("ERROR [%d:%d] ::%s\n", token.line, token.line_offset, msg);
+};
+
+static token_t peek(const parser_t *parser)
+{
+	return parser->tokens[parser->currentToken];
+};
+static token_t peekPrev(const parser_t *parser)
+{
+	i32 index = parser->currentToken;
+	if (index > 0)
+		index = index - 1;
+	return parser->tokens[index];
+};
+static inline bool isAtEnd(const parser_t *parser)
+{
+	token_t token = peek(parser);
+	return token.type == EOF;
+};
+static inline bool check(const parser_t *parser, tokenType_t type)
+{
+	if (isAtEnd(parser))
+		return false;
+	return (peek(parser).type == type);
+};
+static token_t advance(parser_t *parser)
+{
+	if (!isAtEnd(parser))
+		parser->currentToken ++;
+	return peekPrev(parser);
+};
+static bool match(parser_t *parser, const tokenType_t *types, u32 typeCount)
+{
+	for (u32 i = 0; i < typeCount; i++)
+	{
+		if (check(parser, types[i]))
+		{
+			advance(parser);
+			return true;
+		}
+	};
+	return false;
+};
+static bool consume(parser_t *parser, tokenType_t type, const char *msg)
+{
+	if (check(parser, type))
+	{
+		advance(parser);
+		return true;
+	};
+	error(parser, peekPrev(parser), msg);
+	return false;
+};
+
+//Pre-declare basic expression function
+// Yaaaaaaaaay recursion and functional programming
+static expr_t* expression(parser_t *parser);
+
+static expr_t* primary(parser_t *parser)
+{
+	// Check for boolean values, strings, NULL, and numbers
+	{
+		const tokenType_t types[] = { TOKEN_FALSE, TOKEN_TRUE, TOKEN_NIL, TOKEN_NUMBER, TOKEN_STRING, TOKEN_IDENTIFIER };
+		if (match(parser, types, static_len(types)))
+		{
+			token_t value = peekPrev(parser);
+
+			expr_t *lit = arrayAlloc(expr_t, &parser->expressions);
+			lit->type = EXPR_LITERAL;
+			lit->literal.value = value;
+			return lit;
+		}
+	}
+	// Check for groupings
+	{
+		const tokenType_t types[] = { TOKEN_OPEN_PAREN };
+		if (match(parser, types, static_len(types)))
+		{
+			expr_t *expr = expression(parser);
+			if (consume(parser, TOKEN_CLOSE_PAREN, "Expected ')' to close expression"))
+			{
+				expr_t *group = arrayAlloc(expr_t, &parser->expressions);
+				group->type = EXPR_GROUP;
+				group->group.expression = expr;
+				return group;
+			} else {
+				return NULL;
+			}
+		}
+	}
+	return NULL;
+};
+static expr_t* unary(parser_t *parser)
+{
+	// Unary checks for '!' and '-'
+	const tokenType_t types[] = { TOKEN_BANG, TOKEN_MINUS };
+	if (match(parser, types, static_len(types)))
+	{
+		token_t operator = peekPrev(parser);
+		expr_t *right = unary(parser);
+
+		expr_t *un = arrayAlloc(expr_t, &parser->expressions);
+		un->type = EXPR_UNARY;
+		un->unary.operator = operator;
+		un->unary.right = right;
+		return un;
+	};
+	return primary(parser); 
+};
+static expr_t* multiplication(parser_t *parser)
+{
+	expr_t *expr = unary(parser);
+	if (expr)
+	{
+		// Multiplication checks for '*' and '/'
+		const tokenType_t types[] = { TOKEN_STAR, TOKEN_SLASH };
+		while (match(parser, types, static_len(types)))
+		{
+			token_t operator = peekPrev(parser);
+			expr_t *right = unary(parser);
+
+			expr_t *mult = arrayAlloc(expr_t, &parser->expressions);
+			mult->type = EXPR_BINARY;
+			mult->binary.left = expr;
+			mult->binary.operator = operator;
+			mult->binary.right = right;
+
+			expr = mult;
+		};
+	}
+	return expr;
+};
+static expr_t* addition(parser_t *parser)
+{
+	expr_t *expr = multiplication(parser);
+	if (expr)
+	{
+		// Addition checks for '+' and '-'
+		const tokenType_t types[] = { TOKEN_PLUS, TOKEN_MINUS };
+		while (match(parser, types, static_len(types)))
+		{
+			token_t operator = peekPrev(parser);
+			expr_t *right = multiplication(parser);
+
+			expr_t *add = arrayAlloc(expr_t, &parser->expressions);
+			add->type = EXPR_BINARY;
+			add->binary.left = expr;
+			add->binary.operator = operator;
+			add->binary.right = right;
+
+			expr = add;
+		};
+	}
+	return expr;
+};
+static expr_t* comparison(parser_t *parser)
+{
+	expr_t *expr = addition(parser);
+	if (expr)
+	{
+		// Comparison checks for >, >=, <, <=, &&, and ||
+		const tokenType_t types[] =
+		{ 
+			TOKEN_GREATER, TOKEN_GREATER_EQUAL, 
+			TOKEN_LESS, TOKEN_LESS_EQUAL,
+			TOKEN_AND_AND, TOKEN_OR_OR 
+		};
+		while (match(parser, types, static_len(types)))
+		{
+			token_t operator = peekPrev(parser);
+			expr_t *right = addition(parser);
+
+			expr_t *comp = arrayAlloc(expr_t, &parser->expressions);
+			comp->type = EXPR_BINARY;
+			comp->binary.left = expr;	
+			comp->binary.operator = operator;	
+			comp->binary.right = right;
+
+			expr = comp;	
+		};
+	}
+	return expr;
+};
+static expr_t* equality(parser_t *parser)
+{
+	expr_t *expr = comparison(parser);
+	if (expr)
+	{
+		// Equality checks for != and ==
+		const tokenType_t types[] = { TOKEN_BANG_EQUAL, TOKEN_EQUAL_EQUAL };
+		while (match(parser, types, static_len(types)))
+		{
+			token_t operator = peekPrev(parser);
+			expr_t *right = comparison(parser);
+			
+			expr_t *eq = arrayAlloc(expr_t, &parser->expressions);
+			eq->type = EXPR_BINARY;
+			eq->binary.left = expr;
+			eq->binary.operator = operator;
+			eq->binary.right = right;
+
+			expr = eq;
+		};
+	}
+	return expr;
+};
+static expr_t* expression(parser_t *parser)
+{
+	return equality(parser);
+};
+
+#if 0
+static void synchronize(parser_t *parser)
+{
+	advance(parser);
+	while (!isAtEnd(parser))
+	{
+		if (peekPrev(parser).type == TOKEN_SEMICOLON)
+			return;
+
+		switch (peek(parser).type)
+		{
+			case TOKEN_IF:
+			case TOKEN_LET:
+			case TOKEN_FOR:
+			case TOKEN_WHILE:
+			case TOKEN_RETURN:
+				return;
+
+			default: break;
+		};
+
+		advance(parser);
+	};
+};
+#endif
+
+static stmt_t* statement(parser_t *parser);
+
+static stmt_t* variableDeclaration(parser_t *parser)
+{
+	bool isConstant = true;
+	{
+		const tokenType_t types[] = { TOKEN_VAR };
+		if (match(parser, types, static_len(types)))
+		{
+			isConstant = false;
+		};
+	}
+	if (consume(parser, TOKEN_IDENTIFIER, "Expected variable name"))
+	{
+		token_t name = peekPrev(parser);
+
+		token_t type = {};
+		expr_t *initializer = NULL;
+
+		{
+			const tokenType_t type_decl_types[] = { TOKEN_COLON };
+			const tokenType_t type_inf_types[] = { TOKEN_COLON_EQUAL };
+			const tokenType_t type_equal_types[] = { TOKEN_EQUAL };
+			if (match(parser, type_decl_types, static_len(type_decl_types)))
+			{
+				type = advance(parser);
+				if (match(parser, type_equal_types, static_len(type_equal_types)))
+				{
+					initializer = expression(parser);
+				} else if (isConstant) {
+					error(parser, peekPrev(parser), "Expected initializer for constant variable declaration");
+					return NULL;
+				}
+			}else if (match(parser, type_inf_types, static_len(type_inf_types)))
+			{
+				initializer = expression(parser);
+			};
+		}
+
+		if (consume(parser, TOKEN_SEMICOLON, "Expected ';' after variable declaration"))
+		{
+			stmt_t *stmt = arrayAlloc(stmt_t, &parser->statements);
+			stmt->type = STMT_VAR;
+			stmt->var.name = name;
+			stmt->var.type = type;
+			stmt->var.initializer = initializer;
+			return stmt;
+		};
+	};
+	return NULL;
+};
+static stmt_t* declaration(parser_t *parser)
+{
+	{
+		const tokenType_t types[] = { TOKEN_LET };
+		if (match(parser, types, static_len(types)))
+		{
+			return variableDeclaration(parser);
+		}
+	}
+	return statement(parser);
+}
+static stmt_t* expressionStatement(parser_t *parser)
+{
+	expr_t *expr = expression(parser);
+	if (expr)
+	{
+		if (consume(parser, TOKEN_SEMICOLON, "Expected ';' after expression"))
+		{
+			stmt_t *stmt = arrayAlloc(stmt_t, &parser->statements);
+			stmt->type = STMT_EXPR;
+			stmt->expression.expr = expr;
+			return stmt;
+		};
+	};
+	return NULL;
+};
+static stmt_t* statement(parser_t *parser)
+{
+	return expressionStatement(parser);
+};
+
 static void printToken(const char *code, const token_t *token)
 {
 	char *type;
@@ -83,293 +493,6 @@ static void printToken(const char *code, const token_t *token)
 		printf("[%d:%d]\t:: %s\n", token->line, token->line_offset, type);
 	}
 };
-
-/* Abstract Syntax Tree structures */
-struct expr_s;
-typedef struct expr_s expr_t;
-
-typedef struct
-{
-	expr_t *expression;
-} exprGroup_t;
-
-typedef struct
-{
-	token_t operator;
-	expr_t *right;
-} exprUnary_t;
-
-typedef struct
-{
-	token_t operator;
-	expr_t *left;
-	expr_t *right;
-} exprBinary_t;
-
-typedef struct
-{
-	token_t value;
-} exprLiteral_t;
-
-typedef enum
-{
-	EXPR_NONE,
-	EXPR_GROUP,
-	EXPR_UNARY,
-	EXPR_BINARY,
-	EXPR_LITERAL,
-} exprType_t;
-struct expr_s
-{
-	exprType_t type;
-	union
-	{
-		exprGroup_t group;
-		exprUnary_t unary;
-		exprBinary_t binary;
-		exprLiteral_t literal;
-	};
-};
-
-/* Recursive descent parser */
-typedef struct
-{
-	u32 currentToken;
-	const char *code;
-	const token_t *tokens;
-
-	u32 expressionSize;
-	u32 expressionCount;
-	expr_t *expressions;
-} parser_t;
-
-static void error(const parser_t *parser, token_t token, const char *msg)
-{
-	printToken(parser->code, &token);
-	printf("ERROR [%d:%d] ::%s\n", token.line, token.line_offset, msg);
-};
-
-static expr_t* allocExpr(parser_t *parser)
-{
-	// TODO: Better allocation scheme
-	// If we've used all allocated expressions
-	if ((parser->expressionCount + 1) >= parser->expressionSize)
-	{
-		// Double the array length
-		parser->expressionSize <<= 1;
-		// Reallocate the array
-		parser->expressions = realloc(parser->expressions, parser->expressionSize*sizeof(expr_t));
-	};
-	// Get the current index and increment it
-	const u32 index = parser->expressionCount ++;
-	// Return the current expression to use
-	return (parser->expressions + index);
-};
-
-static token_t peek(const parser_t *parser)
-{
-	return parser->tokens[parser->currentToken];
-};
-static token_t peekPrev(const parser_t *parser)
-{
-	i32 index = parser->currentToken;
-	if (index > 0)
-		index = index - 1;
-	return parser->tokens[index];
-};
-static inline bool isAtEnd(const parser_t *parser)
-{
-	token_t token = peek(parser);
-	return token.type == EOF;
-};
-static inline bool check(const parser_t *parser, tokenType_t type)
-{
-	if (isAtEnd(parser))
-		return false;
-	return (peek(parser).type == type);
-};
-static token_t advance(parser_t *parser)
-{
-	if (!isAtEnd(parser))
-		parser->currentToken ++;
-	return peekPrev(parser);
-};
-static bool match(parser_t *parser, const tokenType_t *types, u32 typeCount)
-{
-	for (u32 i = 0; i < typeCount; i++)
-	{
-		if (check(parser, types[i]))
-		{
-			advance(parser);
-			return true;
-		}
-	};
-	return false;
-};
-static bool consume(parser_t *parser, tokenType_t type, const char *msg)
-{
-	if (check(parser, type))
-	{
-		advance(parser);
-		return true;
-	};
-	error(parser, peekPrev(parser), msg);
-	return false;
-};
-
-//Pre-declare basic expression function
-// Yaaaaaaaaay recursion and functional programming
-static expr_t* expression(parser_t *parser);
-
-static expr_t* primary(parser_t *parser)
-{
-	// Check for boolean values, strings, NULL, and numbers
-	{
-		const tokenType_t types[] = { TOKEN_FALSE, TOKEN_TRUE, TOKEN_NIL, TOKEN_NUMBER, TOKEN_STRING };
-		if (match(parser, types, static_len(types)))
-		{
-			token_t value = peekPrev(parser);
-
-			expr_t *lit = allocExpr(parser);
-			lit->type = EXPR_LITERAL;
-			lit->literal.value = value;
-			return lit;
-		}
-	}
-	// Check for groupings
-	{
-		const tokenType_t types[] = { TOKEN_OPEN_PAREN };
-		if (match(parser, types, static_len(types)))
-		{
-			expr_t *expr = expression(parser);
-			if (consume(parser, TOKEN_CLOSE_PAREN, "Expected ')' to close expression"))
-			{
-				expr_t *group = allocExpr(parser);
-				group->type = EXPR_GROUP;
-				group->group.expression = expr;
-				return group;
-			} else {
-				return NULL;
-			}
-		}
-	}
-	error(parser, peek(parser), "Expected expression");
-	return NULL;
-};
-static expr_t* unary(parser_t *parser)
-{
-	// Unary checks for '!' and '-'
-	const tokenType_t types[] = { TOKEN_BANG, TOKEN_MINUS };
-	if (match(parser, types, static_len(types)))
-	{
-		token_t operator = peekPrev(parser);
-		expr_t *right = unary(parser);
-
-		expr_t *un = allocExpr(parser);
-		un->type = EXPR_UNARY;
-		un->unary.operator = operator;
-		un->unary.right = right;
-		return un;
-	};
-	return primary(parser); 
-};
-static expr_t* multiplication(parser_t *parser)
-{
-	expr_t *expr = unary(parser);
-	if (expr)
-	{
-		// Multiplication checks for '*' and '/'
-		const tokenType_t types[] = { TOKEN_STAR, TOKEN_SLASH };
-		while (match(parser, types, static_len(types)))
-		{
-			token_t operator = peekPrev(parser);
-			expr_t *right = unary(parser);
-
-			expr_t *mult = allocExpr(parser);
-			mult->type = EXPR_BINARY;
-			mult->binary.left = expr;
-			mult->binary.operator = operator;
-			mult->binary.right = right;
-
-			expr = mult;
-		};
-	}
-	return expr;
-};
-static expr_t* addition(parser_t *parser)
-{
-	expr_t *expr = multiplication(parser);
-	if (expr)
-	{
-		// Addition checks for '+' and '-'
-		const tokenType_t types[] = { TOKEN_PLUS, TOKEN_MINUS };
-		while (match(parser, types, static_len(types)))
-		{
-			token_t operator = peekPrev(parser);
-			expr_t *right = multiplication(parser);
-
-			expr_t *add = allocExpr(parser);
-			add->type = EXPR_BINARY;
-			add->binary.left = expr;
-			add->binary.operator = operator;
-			add->binary.right = right;
-
-			expr = add;
-		};
-	}
-	return expr;
-};
-static expr_t* comparison(parser_t *parser)
-{
-	expr_t *expr = addition(parser);
-	if (expr)
-	{
-		// Comparison checks for >, >=, <, and <=
-		const tokenType_t types[] = { TOKEN_GREATER, TOKEN_GREATER_EQUAL, TOKEN_LESS, TOKEN_LESS_EQUAL };
-		while (match(parser, types, static_len(types)))
-		{
-			token_t operator = peekPrev(parser);
-			expr_t *right = addition(parser);
-
-			expr_t *comp = allocExpr(parser);
-			comp->type = EXPR_BINARY;
-			comp->binary.left = expr;	
-			comp->binary.operator = operator;	
-			comp->binary.right = right;
-
-			expr = comp;	
-		};
-	}
-	return expr;
-};
-static expr_t* equality(parser_t *parser)
-{
-	expr_t *expr = comparison(parser);
-	if (expr)
-	{
-		// Equality checks for != and ==
-		const tokenType_t types[] = { TOKEN_BANG_EQUAL, TOKEN_EQUAL_EQUAL };
-		while (match(parser, types, static_len(types)))
-		{
-			token_t operator = peekPrev(parser);
-			expr_t *right = comparison(parser);
-			
-			expr_t *eq = allocExpr(parser);
-			eq->type = EXPR_BINARY;
-			eq->binary.left = expr;
-			eq->binary.operator = operator;
-			eq->binary.right = right;
-
-			expr = eq;
-		};
-	}
-	return expr;
-};
-static expr_t* expression(parser_t *parser)
-{
-	return equality(parser);
-};
-
 static void printExpression(const char *code, const expr_t *expr, u32 index)
 {
 	for(u32 i = 0; i < index; i++)
@@ -407,16 +530,32 @@ static void printExpression(const char *code, const expr_t *expr, u32 index)
 		} break;
 	};
 };
+static void printStatement(const char *code, const stmt_t *stmt)
+{
+	switch (stmt->type)
+	{
+		case STMT_NONE: break;
+		case STMT_EXPR:
+		{
+			printExpression(code, stmt->expression.expr, 0);
+		} break;
+		case STMT_VAR:
+		{
+			printToken(code, &stmt->var.name);
+			printToken(code, &stmt->var.type);
+			if (stmt->var.initializer)
+			{
+				printExpression(code, stmt->var.initializer, 0);
+			}
+		};
+	};
+};
 
 i32 parse(const char *code, const token_t *tokens, u32 tokenCount)
 {
 	parser_t parser = {};
 	parser.code = code;
 	parser.tokens = tokens;
-
-	parser.expressionCount = 0;
-	parser.expressionSize = 16;
-	parser.expressions = malloc(parser.expressionSize*sizeof(expr_t));
 
 	#if 0
 	for (u32 i = 0; i < tokenCount; i++)
@@ -426,13 +565,14 @@ i32 parse(const char *code, const token_t *tokens, u32 tokenCount)
 	}
 	#endif
 
-	expr_t *expr = expression(&parser);
-	while (expr)
+	stmt_t *stmt = declaration(&parser);
+	while (stmt)
 	{
-		printExpression(code, expr, 0);
-		expr = expression(&parser);
+		printStatement(code, stmt);
+		stmt = declaration(&parser);
 	};
 
-	free(parser.expressions);
+	freeArray(stmt_t, &parser.statements);
+	freeArray(expr_t, &parser.expressions);
 	return 0;
 }
--- src/util.h
+++ src/util.h
@@ -28,4 +28,34 @@ i32 	stringToInt(const char *string, size_t size);
 // TODO: Move this
 u8* loadEntireFile(const char *path, size_t *size);
 
+// Preprocessor macro for declaring an array of something, i.e. arrayOf_int_t
+#define arrayOf(__type__)				arrayOf_##__type__##_t
+// Allocates a member of an array of something
+#define arrayAlloc(__type__, array)	arrayAlloc_##__type__(array)
+// Frees an array of something
+#define freeArray(__type__, array)	freeArray_##__type__(array)
+// Declares the interface for an array of something
+#define declareArrayOf(__type__) \
+	typedef struct \
+	{ \
+		u32 used, size;\
+		__type__ *data;\
+	} arrayOf(__type__);\
+	__type__* arrayAlloc_##__type__(arrayOf(__type__) *array) {\
+		if (!array->data) {\
+			array->used = 0;\
+			array->size = 16;\
+			array->data = malloc(array->size*sizeof(__type__));\
+		};\
+		if ((array->used + 1) >= array->size){\
+			array->size <<= 1;\
+			array->data = realloc(array->data, array->size*sizeof(__type__));\
+		};\
+		const u32 index = array->used++;\
+		return (array->data + index);\
+	};\
+	void freeArray_##__type__(arrayOf(__type__) *array) {\
+		if (array->data) free(array->data);\
+	}
+
 #endif
